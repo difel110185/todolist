3e62b1d9a19a3104bcbf25427c7c11f153548b60
Diego Felix <dfelix@wkm.com.br>
2020-02-13 16:15:02 -0800

    Adding Add Todo, Complete Todo and Delete Todo features

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,11 @@ import './TodoItem.css';
 
 function TodoItem(props) {
     return (
-        <li className={props.important && "important"}><input type={"checkbox"} /> {props.name}</li>
+        <li key={props.id} className={props.important && "important"}>
+            {props.complete ? <input type={"checkbox"} checked={true} onChange={() => props.toggleComplete(props.id)} /> : <input type={"checkbox"} onChange={() => props.toggleComplete(props.id)} />}
+            {props.name}
+            {props.complete && <button className={"button"} onClick={() => props.deleteTodo(props.id)}>X</button>}
+        </li>
     );
 }
 
